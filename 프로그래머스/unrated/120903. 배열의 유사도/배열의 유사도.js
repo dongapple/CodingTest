aeef22cfcafaeab90b644a736f5ccd078f7d79e2
신동혁 <74224516+dongapple@users.noreply.github.com>
2023-10-12 23:48:05 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,3 @@
 function solution(s1, s2) {
-    let answer = 0;
-    for(const char1 of s1){
-        for(const char2 of s2){
-            if(char1==char2) answer++;   
-        }
-    }
-    return answer;
+    return s1.filter((v)=>s2.includes(v)).length
 }
