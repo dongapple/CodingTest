400b04d30014793ec7e26dace1a68b420e6abbdc
신동혁 <74224516+dongapple@users.noreply.github.com>
2023-11-03 16:01:26 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,8 +1,7 @@
 function solution(num_list) {
    let answer =[0,0]
    for (const num of num_list){
-       if(!(num%2)){answer[0] = answer[0] + 1}
-       else if(num%2){answer[1] = answer[1] + 1}
+       answer[num%2]++
    }
     return answer
 }
