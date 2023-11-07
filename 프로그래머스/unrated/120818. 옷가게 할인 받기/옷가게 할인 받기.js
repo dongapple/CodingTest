bb2e588a53e9600b3eb7256a31258acb5c556216
신동혁 <74224516+dongapple@users.noreply.github.com>
2023-11-08 02:03:49 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -2,5 +2,5 @@ function solution(price) {
     return  price>=500000? Math.trunc(price*0.8,0):
             price>=300000? Math.trunc(price*0.9,0):
             price>=100000? Math.trunc(price*0.95,0):
-            price*1;
+            price;
 }
